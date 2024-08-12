f8f870cf9f200ffaf6c2c37a4a5daf597bea8e41
Ugur Arikan (DHL Data & Analytics) <ugur.arikan@dpdhl.com>
2024-08-12 08:11:12 +0200

    fill-with variants to initialize memory on growth
    `into_concurrent_filled_with` and `grow_to_and_fill_with` methods are required to enable data structures which always have an initialized and valid state.

--- src/into_concurrent_pinned_vec.rs
+++ src/into_concurrent_pinned_vec.rs
@@ -7,4 +7,14 @@ pub trait IntoConcurrentPinnedVec<T>: PinnedVec<T> {
 
     /// Converts the pinned vector into its concurrent wrapper.
     fn into_concurrent(self) -> Self::ConPinnedVec;
+
+    /// Converts the pinned vector into its concurrent wrapper.
+    /// During conversion:
+    ///
+    /// * length of the vector is increased to its capacity;
+    /// * the elements in the range `len..capacity` are filled with the values
+    /// obtained by repeatedly calling the function `fill_with`.
+    fn into_concurrent_filled_with<F>(self, fill_with: F) -> Self::ConPinnedVec
+    where
+        F: Fn() -> T;
 }
